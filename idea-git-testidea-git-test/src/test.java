6b45b607308089bae636eebbbc1e221709208715
xingzou <fmhlife@outlook.com>
2018-11-12 20:29:10 +0800

    第四次提交，更改数值

--- idea-git-testidea-git-test/src/test.java
+++ idea-git-testidea-git-test/src/test.java
@@ -2,8 +2,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaaaa");
         System.out.println("bbbbb");
-        int a = 3;
-        int b = 4;
+        int a = 2;
+        int b = 5;
         int c = a + b;
         System.out.println(c);
     }
