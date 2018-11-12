9c49a55881df322790d136c47aa4947bd67004ce
xingzou <fmhlife@outlook.com>
2018-11-12 19:59:55 +0800

    第二次提交

--- idea-git-testidea-git-test/src/test.java
+++ idea-git-testidea-git-test/src/test.java
@@ -2,8 +2,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaaaa");
         System.out.println("bbbbb");
-        int a = 1;
-        int b = 2;
+        int a = 3;
+        int b = 4;
         int c = a + b;
     }
 }
