c880a02d818ce7ed25af2e33d93e0d47c379772f
云中行走 <40264555+fmhlife@users.noreply.github.com>
2018-11-12 19:49:19 +0800

    Update test.java

--- idea-git-testidea-git-test/src/test.java
+++ idea-git-testidea-git-test/src/test.java
@@ -2,5 +2,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaaaa");
         System.out.println("bbbbb");
+        int a = 1;
+        int b = 2;
+        int c = a + b;
     }
 }
